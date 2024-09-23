78fefb44d6d05c5052db616bb9bf6c5c3071e873
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-22 22:58:26 -0300

    local database for test

--- crud-spring/src/main/java/com/victor/crud_spring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/victor/crud_spring/CrudSpringApplication.java
@@ -1,7 +1,11 @@
 package com.victor.crud_spring;
 
+import com.victor.crud_spring.model.Course;
+import com.victor.crud_spring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +14,17 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	//bd h2 rodará aqui
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
